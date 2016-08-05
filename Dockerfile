d59d8fdfa6172216bd2818e54670b87f79948d3f
Goffert van Gool <goffert@phusion.nl>
2016-08-05 12:47:42 +0200

    Update for docker 1.12 release

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ RUN mkdir /mech
 RUN apk add --no-cache --virtual .docker tar curl ca-certificates \
 	&& curl https://get.docker.com/builds/Linux/x86_64/docker-latest.tgz | tar -xz docker/docker --strip-components=1 \
 	&& mv docker /usr/bin/docker \
+	&& curl https://get.docker.com/builds/Linux/x86_64/docker-1.11.2.tgz | tar -xz usr/local/bin/docker --strip-components=3 \
+	&& mv docker /usr/bin/docker1.11 \
 	&& curl https://get.docker.com/builds/Linux/x86_64/docker-1.10.3.tgz | tar -xz usr/local/bin/docker --strip-components=3 \
 	&& mv docker /usr/bin/docker1.10 \
 	&& curl https://get.docker.com/builds/Linux/x86_64/docker-1.9.1.tgz | tar -xz usr/local/bin/docker --strip-components=3 \
--- bin/mech.rb
+++ bin/mech.rb
@@ -10,17 +10,18 @@ $stdout.sync = true
 
 # A map of docker Server API versions to client versions
 VERSION_MAP = {
-	'1.21' => '1.9',
-	'1.22' => '1.10'
+  '1.21' => '1.9',
+  '1.22' => '1.10',
+  '1.23' => '1.11'
 }
 
 # Swap to the correct docker client
 version_error =`docker version 2>&1`.split("\n").last[/Error response from daemon: (client and server don't have same version|client is newer than server).*/]
 if version_error
-	server_api_version = version_error[/server\sAPI\sversion:\s\d\.\d\d/][/\d\.\d\d/]
-	client_version = VERSION_MAP[server_api_version]
-	File.delete('/usr/bin/docker')
-	File.symlink("/usr/bin/docker#{client_version}", '/usr/bin/docker')
+  server_api_version = version_error[/server\sAPI\sversion:\s\d\.\d\d/][/\d\.\d\d/]
+  client_version = VERSION_MAP[server_api_version]
+  File.delete('/usr/bin/docker')
+  File.symlink("/usr/bin/docker#{client_version}", '/usr/bin/docker')
 end
 
 Dir.glob('/mech/managers/*.rb') { |file| require file }
